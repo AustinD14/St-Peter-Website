8b128e4585dcd2890cbd39cd1867391e456c7a66
Austin <austindimalanta14@gmail.com>
2021-06-23 15:34:47 -0700

    adding about us page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react"
 import "style.css"
 import "tailwindcss/dist/base.css"
 import LandingPage from "components/LandingPage";
+import AboutUs from "components/AboutUs";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 
 function App() {
   return (
-    <Router>
+    <Router basename="/St-Peter-Website">
         <Switch>
           <Route exact path="/" component={LandingPage} />
+          <Route path="/AboutUs" component={AboutUs} />
         </Switch>
   </Router>
   )
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -20,6 +20,7 @@ export default () => (
     <Hero />
     <MainFeature />
     <Services />
+    WOW
     {/*<MainFeature2 />
     <Portfolio />
     <Testimonial
