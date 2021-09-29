24964f7bdd93d318abcd2d99dd8a6dd5fc42b060
Austin <austindimalanta14@gmail.com>
2021-09-28 18:03:19 -0700

    added links to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -9,22 +9,16 @@ import MainFeature from "components/LandingPage/Feature1";
 import MainFeature2 from "components/LandingPage/Feature2";
 import MainFeature3 from "components/LandingPage/Feature3";
 import MainFeature4 from "components/LandingPage/Feature4";
-import Portfolio from "components/cards/PortfolioTwoCardsWithImage.js";
-import Blog from "components/blogs/ThreeColSimpleWithImageAndDashedBorder.js";
-import Testimonial from "components/testimonials/TwoColumnWithImageAndProfilePictureReview.js";
-import FAQ from "components/faqs/SimpleWithSideImage.js";
-import ContactUsForm from "components/forms/TwoColContactUsWithIllustration.js";
 import Footer from "components/LandingPage/Footer";
-import customerSupportIllustrationSrc from "images/customer-support-illustration.svg";
 
 export default () => (
   <AnimationRevealPage>
-    <Hero />
-    <MainFeature />
-    <MainFeature2 />
-    <MainFeature3 />
-    <MainFeature4 />
-    <Services />
-    <Footer /> 
+    <Hero primaryButtonUrl="/Reviews"/>
+    <MainFeature primaryButtonUrl="/AboutUs" />
+    <MainFeature2 primaryButtonUrl="/AboutUs" />
+    <MainFeature3 primaryButtonUrl="/AboutUs"/>
+    <MainFeature4 primaryButtonUrl="/AboutUs"/>
+    {/* <Services /> */}
+    <Footer />
   </AnimationRevealPage>
 );
